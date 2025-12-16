8ad899cb5dacf626997e15b3adb1c52e9e04497d
emboem <94947467+emboem@users.noreply.github.com>
2025-12-16 20:45:47 +0700

    Update fastmoss_web.py

--- fastmoss_web.py
+++ fastmoss_web.py
@@ -178,13 +178,14 @@ class FastMossScraper:
             cat_string = " > ".join(item.get("all_category_name", []) or [])
             price_raw = item.get('real_price', 0)
             growth_raw = item.get('sold_count_inc_rate', 0)
+            created_time = item.get('ctime', None)
             
             parsed_data.append({
                 "Judul": item.get("title"),
                 "Harga Display": price_raw, 
                 "Kategori": cat_string,
                 "Toko": shop_name,
-                "Tanggal dibuat": item.get("ctime"),
+                "Tanggal dibuat": datetime.fromtimestamp(created_time).strftime("%Y-%m-%d") if created_time else "-",
                 "Terjual (Periode)": item.get("sold_count_show"),
                 "Omzet (Periode)": item.get("sale_amount_show"),
                 "Terjual (Total)": item.get("total_sold_count_show"),
